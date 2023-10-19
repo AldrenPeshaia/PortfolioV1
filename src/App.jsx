41afe23a81386fe8d0c73c588532230c26d4f8b4
AldrenPeshaia <113583715+AldrenPeshaia@users.noreply.github.com>
2023-10-19 08:16:20 +0200

    Added Browser routing and also tailwindcss to the config and as a dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <>dsd</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route />
+          <Route />
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
